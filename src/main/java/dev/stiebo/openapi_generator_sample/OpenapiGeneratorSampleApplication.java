a7d7b7c0d7cd7688513971f178f3d42d8fb7df1a
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-11-19 09:39:45 +0100

    Initial commit - ready to generate from openapi

---
+++ src/main/java/dev/stiebo/openapi_generator_sample/OpenapiGeneratorSampleApplication.java
@@ -0,0 +1,13 @@
+package dev.stiebo.openapi_generator_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiGeneratorSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiGeneratorSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/stiebo/openapi_generator_sample/OpenapiGeneratorSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stiebo.openapi_generator_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
